25dae8803af7c69cc1b297bf41ade77d160093d4
T0mii <tomashoyosd9@gmail.com>
2026-03-09 18:44:11 -0500

    Update models_valoraciones.py

--- marketplace/models_valoraciones.py
+++ marketplace/models_valoraciones.py
@@ -1,9 +1,11 @@
 from django.db import models
+
+from django.conf import settings
 from .models_servicios import ServicioFreelance
 
 class ServicioFreelanceValoracion(models.Model):
     servicio = models.ForeignKey(ServicioFreelance, on_delete=models.CASCADE, related_name='valoraciones')
-    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     calificacion = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
     comentario = models.TextField(blank=True)
     creado = models.DateTimeField(auto_now_add=True)
